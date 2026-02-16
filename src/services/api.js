bb0b8abd81fcaa2371d648743dd14add957b10ea
Darksoullllll <ishugautam2003@gmail.com>
2026-02-16 20:20:52 +0530

    minor fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://willowy-selma-myself22-adad783f.koyeb.app/api/",
 });
 
 export const getTasks = () => API.get("tasks/");
